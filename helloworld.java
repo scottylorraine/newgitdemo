686f8bdeadc410a452acfc8857b34ad41e2d4f3c
ananth <scottylorrain62@gmail.com>
2022-08-19 11:26:17 +0000

    deleted hello1.jva and helloworld.jva

--- helloworld.java
+++
@@ -1,7 +0,0 @@
-public class helloworld {
-               // helloworldprogram//
-           public static void main(string[] args) {
-               system.out.println("hllonewworld");
-                 //ending of the program//
-        }
-}
